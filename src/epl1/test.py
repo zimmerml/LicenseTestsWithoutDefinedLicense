8fc900f2e448d85f68ebf6f2891aab40a90da55c
Michael Zimmermann <micha89@gmail.com>
2022-12-15 16:40:44 +0100

    update

---
+++ src/epl1/test.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 1.0 which is available at
+# https://www.eclipse.org/legal/epl-v10.html.
+#
+# SPDX-License-Identifier: EPL-1.0
+
+print("TEST")
